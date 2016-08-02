ae017a02abb32517198805e93f15ca9485fe52d8
Artal Druk <artald@wix.com>
2016-08-02 11:41:01 +0300

    Fixed issue with initial value not adjusting the size on Android (related to issue #1) This is a temporary JS hack for Android (temporary hack for iOS was implemented natively). After moving to RN > 30 we can probably remove all the hacks.

--- example/index.android.js
+++ example/index.android.js
@@ -3,49 +3,82 @@
  * https://github.com/facebook/react-native
  */
 
-import React, {
+import React, {Component} from 'react';
+
+import {
   AppRegistry,
-  Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
+import {AutoGrowingTextInput} from 'react-native-autogrow-textinput';
+
 class example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {textValue: 'My initial Text'};
+  }
+  
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Auto Growing TextInput Example
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <AutoGrowingTextInput value={this.state.textValue}
+                              onChange={(event) => this._onChange(event)}
+                              style={styles.textInput}
+                              placeholder={'Your Message'}
+                              minHeight={40}
+                              maxHeight={200}
+                              ref={(r) => { this._textInput = r; }}
+        />
+        <TouchableOpacity style={styles.button} onPress={() => this._resetTextInput()}>
+          <Text>Clear</Text>
+        </TouchableOpacity>
       </View>
     );
+  }
+  
+  _onChange(event) {
+    this.setState({ textValue: event.nativeEvent.text || '' });
+  }
+  
+  _resetTextInput() {
+    this._textInput.setNativeProps({text: ''});
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#76c6ff'
   },
   welcome: {
+    marginTop: 100,
     fontSize: 20,
     textAlign: 'center',
+    margin: 10
+  },
+  textInput: {
+    //height: 35,
     margin: 10,
+    paddingLeft: 10,
+    fontSize: 17,
+    lineHeight: 35,
+    backgroundColor: 'white',
+    borderWidth: 0,
+    borderRadius: 4
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  button: {
+    alignItems: 'center',
+    width: 100,
+    height: 80,
+    marginTop: 50
+  }
 });
 
 AppRegistry.registerComponent('example', () => example);
--- src/AutoGrowingTextInput.js
+++ src/AutoGrowingTextInput.js
@@ -6,7 +6,9 @@ import {
   TextInput,
   StyleSheet,
   LayoutAnimation,
-  NativeModules
+  NativeModules,
+  Platform,
+  Text
 } from 'react-native';
 
 var AutoGrowTextInputManager = NativeModules.AutoGrowTextInputManager;
@@ -16,27 +18,48 @@ const DEFAULT_ANIM_DURATION = 100;
 export default class AutoGrowingTextInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {height: this._getValidHeight(props.initialHeight)};
-    //using a native fix for "onChange" not received when the text is set pragmatically and not by the user (https://github.com/wix/react-native-autogrow-textinput/issues/1)
+  
+    // using a JS fix for "onChange" not received on Android when the text is set pragmatically and not by the user
+    // a hidden Text is used to measure the height in case it's received via the value prop
+    this.state = {height: this._getValidHeight(props.initialHeight),
+                  androidHacksHiddenText: props.value || '',
+                  androidHacksSetHeightFromHiddenLayout: true};
+    
+    // using a native fix for "onChange" not received on iOS when the text is set pragmatically and not by the user
+    // (https://github.com/wix/react-native-autogrow-textinput/issues/1)
     if(AutoGrowTextInputManager) {
       AutoGrowTextInputManager.setupNotifyChangeOnSetText();
     }
   }
-
-  render() {
+  
+  componentWillReceiveProps(props) {
+    if(Platform.OS === 'android') {
+      this._handleAndroidHackProps(props);
+    }
+  }
+  
+  _renderAutoGrowingTextInput() {
     return (
       <TextInput multiline={true}
-        {...this.props} {...this.style}
+                 {...this.props} {...this.style}
                  style={[this.props.style, {height: this._getValidHeight(this.state.height)}]}
                  onChange={(event) => {
-                    this._onChangeNativeEvent(event.nativeEvent);
-                    if (this.props.onChange) {
-                      this.props.onChange(event);
-                    }
+                   this._onChangeNativeEvent(event.nativeEvent);
+                   if (this.props.onChange) {
+                     this.props.onChange(event);
+                   }
                  }}
                  ref={(r) => { this._textInput = r; }}
       />
     );
+  }
+  
+  render() {
+    if(Platform.OS === 'ios') {
+      return this._renderAutoGrowingTextInput();
+    } else {
+      return this._renderAutoGrowingTextInputAndroidHack();
+    }
   }
 
   _getValidHeight(height) {
@@ -68,6 +91,10 @@ export default class AutoGrowingTextInput extends React.Component {
 
   setNativeProps(nativeProps = {}) {
     this._textInput.setNativeProps(nativeProps);
+    
+    if(Platform.OS === 'android') {
+      this._handleAndroidHackProps({value: nativeProps.text}, true);
+    }
   }
 
   resetHeightToMin() {
@@ -75,7 +102,69 @@ export default class AutoGrowingTextInput extends React.Component {
       height: this.props.minHeight
     });
   }
+  
+  /*
+     Android Hacks
+   */
+  
+  _resetAndroidHacks(text = '') {
+    this.setState({androidHacksHiddenText: text, androidHacksSetHeightFromHiddenLayout: true});
+  }
+  
+  _handleAndroidHackProps(props, forceUpdate = false) {
+    if(!props.value) {
+      this._resetAndroidHacks();
+    } else if(this.state.androidHacksHiddenText !== props.value && (this.state.androidHacksSetHeightFromHiddenLayout || forceUpdate)) {
+      this.setState({androidHacksHiddenText: props.value, androidHacksSetHeightFromHiddenLayout: true});
+    }
+  }
+  
+  _renderAutoGrowingTextInputAndroidHack() {
+    return (
+      <View style={{flex: 1}}>
+        <TextInput multiline={true}
+                   {...this.props} {...this.style}
+                   style={[this.props.style, {height: this._getValidHeight(this.state.height)}]}
+                   onChange={(event) => {
+                     if(this.state.androidHacksSetHeightFromHiddenLayout) {
+                       this.setState({androidHacksHiddenText: event.nativeEvent.text});
+                     } else {
+                       this._onChangeNativeEvent(event.nativeEvent);
+                       if (this.props.onChange) {
+                         this.props.onChange(event);
+                       }
+                     }
+                   }}
+                   ref={(r) => { this._textInput = r; }}
+        />
+        <Text style={[this.props.style, styles.hiddenOffScreen]}
+              onLayout={(event) => {
+                if(this.state.androidHacksSetHeightFromHiddenLayout) {
+                  event.nativeEvent = {contentSize: {height: event.nativeEvent.layout.height, width: event.nativeEvent.layout.width}, text: this.state.androidHacksHiddenText};
+                  this._onChangeNativeEvent(event.nativeEvent);
+                  if (this.props.onChange) {
+                    this.props.onChange(event);
+                  }
+                  this.setState({androidHacksSetHeightFromHiddenLayout: false});
+                }
+              }}>
+          {this.state.androidHacksHiddenText}
+        </Text>
+      </View>
+    );
+  }
 }
+
+var styles = StyleSheet.create({
+  hiddenOffScreen: {
+    position: 'absolute',
+    top: 5000,
+    left: 5000,
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    color: 'transparent'
+  }
+});
 
 AutoGrowingTextInput.propTypes = {
   autoGrowing: PropTypes.bool,
