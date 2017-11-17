61c357b6519d64b379d306b1d3776e76c4e920af
Artem Bykov <artem.bikov@gmail.com>
2017-11-17 14:09:48 +0300

    fix for react-native 0.47.2

--- android/src/main/java/com/wix/autogrowtextinput/AutoGrowTextInputPackage.java
+++ android/src/main/java/com/wix/autogrowtextinput/AutoGrowTextInputPackage.java
@@ -23,11 +23,6 @@ public class AutoGrowTextInputPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
